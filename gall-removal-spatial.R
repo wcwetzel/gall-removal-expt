5139383674b6e68eaa49e2733c2c00c428889ab2
wcwetzel <wcwetzel@ucdavis.edu>
2011-10-28 20:23:22 -0700

    starting gall removal spatial analysis

---
+++ gall-removal-spatial.R
@@ -0,0 +1,25 @@
+########################
+## GALL REMOVAL EXP'T ##
+### SPATIAL ANALYSIS ###
+########################
+
+# analysis for 2010-2011 sg1 data
+# with spatial correlations
+
+# 28 Oct 2011
+
+d = read.csv(
+  '~/documents/DATA/2010 DATA/FIELD/sg1/sg1-2010-2011-DONOTCHANGE.csv')
+d = d[-115,]
+
+
+
+
+
+
+
+
+
+
+
+
